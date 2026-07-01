da761cdd6458f89413a45cdff6b05384d8efd119
Akshit Anand <akshitanand1336@gmail.com>
2026-07-01 10:30:40 +0530

    Time: 1 ms (89.05%), Space: 43 MB (79.93%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int capital = 0;
+        for(int i = 0 ; i < word.length() ; i++){
+            char ch = word.charAt(i);
+            if(ch >= 'A' && ch <= 'Z')capital++;
+        }
+        if(capital == 0 || capital == word.length() || (capital == 1 && (word.charAt(0) >= 'A' && word.charAt(0) <= 'Z')))return true;
+        return false;
+    }
+}
